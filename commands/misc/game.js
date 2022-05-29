02f195920f538daa7556fa2e7ef142231b199cf1
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-05-29 00:05:44 -0500

    Update game.js

--- commands/misc/game.js
+++ commands/misc/game.js
@@ -11,7 +11,7 @@ module.exports = {
             message.channel.send("You can't do this.")
             return;
         }
-        
+
         const commandType = args[0].toLowerCase();
         let save = false;
 
@@ -19,16 +19,25 @@ module.exports = {
         nconf.load();
         let players = nconf.get('players');
         let game = nconf.get('game');
+        if(game === undefined)
+        {
+            game = 
+            [
+                started = false
+            ];
+        }
 
         if(commandType == "start")
         {
-            if(game.started)
+            if(game.started != undefined && game.started)
             {
                 message.channel.send("A game already exists!")
             }
             else
             {
-
+                message.channel.send("Starting game.")
+                game.started = true;
+                nconf.set('game', game);
             }
         }
 
