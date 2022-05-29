b2839017379cdc17acdb1e9ff58ed5694bb800a0
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-05-29 12:52:47 -0500

    Update player.js

--- commands/misc/player.js
+++ commands/misc/player.js
@@ -23,35 +23,36 @@ module.exports = {
                 return; // Do not proceed, there is no user.
             }
 
-            let players = {};
             global.con.query('SELECT * FROM `players`',
                 function(err, results, fields) {
-                    players = results;
+                    if(err) 
+                    {
+                        message.channel.send("Error.")
+                        console.log(err);
+                        return;
+                    }
+
+                    let players = results;
+
+                    console.log(players);
+
+                    if(!(user.id in players))
+                    {
+                        global.con.query(`INSERT INTO players (ID, TARGET, ALIVE) values (${user.id}, '0', true)`, (err, row) => {
+                            // Return if there is an error
+                            if (err) {
+                                message.channel.send("Failed");
+                                return console.log(err);
+                            }
+                            else message.channel.send(`Added ${user.username}.`);
+                        });
+                    }
+                    else
+                    {
+                        message.channel.send("That player is already in the game.");
+                    }
                 }
             );
-
-            if(players === undefined)
-            {
-                players = {};
-            }
-
-            console.log(players);
-
-            if(!(user.id in players))
-            {
-                global.con.query(`INSERT INTO players (ID, TARGET, ALIVE) values (${user.id}, '0', true)`, (err, row) => {
-                    // Return if there is an error
-                    if (err) {
-                        message.channel.send("Failed");
-                        return console.log(err);
-                    }
-                    else message.channel.send(`Added ${user.username}.`);
-                });
-            }
-            else
-            {
-                message.channel.send("That player is already in the game.");
-            }
         }
 
         if(commandType == "remove")
