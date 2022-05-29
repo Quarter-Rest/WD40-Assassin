894ff4c977736471fde6fd3bf2882037784aa61c
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-05-29 13:14:13 -0500

    Update player.js

--- commands/misc/player.js
+++ commands/misc/player.js
@@ -103,9 +103,10 @@ module.exports = {
 
 
                 players.forEach(playerData => {
-                    let player = message.client.users.fetch(playerData.ID);
-                    console.log(player);
-                    if(player != undefined) embed.addField("Player", player.User.username, true)
+                    let player = message.client.users.fetch(playerData.ID).then(() => {
+                        console.log(player);
+                        if(player != undefined) embed.addField("Player", player.username, true)
+                    });
                 });
 
                 message.channel.send({embeds: [embed]});
