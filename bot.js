7242d5dadbb9c7c7382de90122dd886482af28c3
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-06-01 13:46:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,7 +101,7 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const command = require(`./commands/${folder}/${file}`);
 		client.commands.set(command.name, command);
-        if(command.timerHandler()) command.timerHandler();
+        if(typeof command.timerHandler === "function") command.timerHandler();
 	}
 }
 
