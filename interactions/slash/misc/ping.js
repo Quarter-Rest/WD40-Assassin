17a2fc7101153976289f03bf726148234099ff68
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-06-01 21:12:10 -0500

    s

---
+++ interactions/slash/misc/ping.js
@@ -0,0 +1,48 @@
+/**
+ * @file Sample help command with slash command.
+ * @author Naman Vrati
+ * @author Thomas Fournier <thomas@artivain.com>
+ * @since 3.0.0
+ * @version 3.1.0
+ */
+
+// Deconstructed the constants we need in this file.
+
+const { MessageEmbed, Collection } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+	// The data needed to register slash commands to Discord.
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription(
+			"Ping and select menu test."
+		),
+
+	async execute(interaction) {
+		/**
+		 * @type {MessageEmbed}
+		 * @description Help command's embed
+		 */
+        const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'Select me',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: 'You can select me too',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+					]),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	},
+};
