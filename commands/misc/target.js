a797cb7636aded49adf8a1df360f5b1a56396dd0
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-06-09 23:54:41 -0500

    Update target.js

--- commands/misc/target.js
+++ commands/misc/target.js
@@ -11,26 +11,25 @@ module.exports = {
             players.some(element => {
                 if (element.id === user.id) {
                     let targetID = element.targetid;
-                    players.some(element2 => {
-                        if (element2.id === targetID) {
-                            console.log(element2)
-                            user.send(`Current Target: ${element2.username}`).then(() => 
-                            {
-                                if (message.channel.type === "dm") return;
-                            })
-                            .catch((error) => 
-                            {
-                                // On failing, throw error.
-                                console.error(
-                                    `Could not send DM to ${player.tag}.\n`,
-                                    error
-                                );
+                    
+                    message.client.users.fetch(targetID).then(target => {
+						targetName = target.username;
+						
+						user.send(`Current Target: ${targetName}`).then(() => 
+						{
+							if (message.channel.type === "dm") return;
+						})
+						.catch((error) => 
+						{
+							// On failing, throw error.
+							console.error(
+								`Could not send DM to ${player.tag}.\n`,
+								error
+							);
 
-                                message.channel.send(`Could not send DM to ${player.tag}.\n`);
-                            });
-                            return;
-                        }
-                    });
+							message.channel.send(`Could not send DM to ${player.tag}.\n`);
+						});
+					})
                     return;
                 }
             });
