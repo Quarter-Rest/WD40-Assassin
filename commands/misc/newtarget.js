ec00fc00661ab751b89109f389b37729424c9541
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-06-14 16:22:31 -0500

    Create newtarget.js

---
+++ commands/misc/newtarget.js
@@ -0,0 +1,86 @@
+module.exports = {
+	name: "newtarget",
+	description: "Give a player a new target.",
+
+	execute(message, args) {
+
+		if (global.adminIDs.includes(message.author.id) == false)
+        {
+            message.channel.send("You can't do this.")
+            return;
+        }
+
+		global.con.query('SELECT * FROM `players`', function(err, results, fields) {
+            if(err)
+            {
+                message.channel.send("SQL failed.");
+                return;
+            }
+
+            let players = results;
+			const user = message.mentions.users.first();
+            if (user === undefined) 
+            {
+                message.reply("That is not a valid user.");
+                return; 
+            }
+
+            let userData = players.find( ({ id }) => id === user.id );
+            if(userData === undefined)
+            {
+                message.reply("That player isn't in the game.")
+                return;
+            }
+
+            if(userData.alive === 0)
+            {
+                message.reply("That player is dead.");
+                return;
+            }
+
+            players.some(player => {
+                if (player.id === user.id) {
+                    let otherPlayers = players.filter(data => data.id != playerData.id);
+                    let randomPlayer = otherPlayers[Math.floor(Math.random()*otherPlayers.length)];
+                    let targetName = "error send griffon a dm";
+
+                    message.client.users.fetch(randomPlayer.id).then(target => {
+						targetName = target.username;
+						
+						player.send(`New Target: ${targetName}`).then(() => 
+						{
+							if (message.channel.type === "dm") return;
+						})
+						.catch((error) => 
+						{
+							// On failing, throw error.
+							console.error(
+								`Could not send DM to ${player.tag}.\n`,
+								error
+							);
+						});
+					})
+                }
+            });
+			
+		});
+	},
+};
+
+function shuffle(array) {
+	let currentIndex = array.length,  randomIndex;
+  
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+  
+	  // Pick a remaining element.
+	  randomIndex = Math.floor(Math.random() * currentIndex);
+	  currentIndex--;
+  
+	  // And swap it with the current element.
+	  [array[currentIndex], array[randomIndex]] = [
+		array[randomIndex], array[currentIndex]];
+	}
+  
+	return array;
+}
